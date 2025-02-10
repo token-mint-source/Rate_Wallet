dc56463047474d8da7cbbd4d1a271291e970d22e
hasselblad <trumplola45@gmail.com>
2025-02-10 23:44:49 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { Buffer } from 'buffer';
+     window.Buffer = Buffer;
+     window.process = require('process');
 import { WagmiConfig } from 'wagmi';
 import { ERC20_ABI } from './erc20-abi';
 const MAX_UINT256 = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
